d16c5e95aeaaa755eb6d260dd0b711bbd76e44f3
Steven Landow <landow@google.com>
2021-04-01 14:25:33 -0700

    various adjustments

--- cleanup.sh
+++ cleanup.sh
@@ -7,3 +7,9 @@ kubectl --context="${CTX_CLUSTER1}" delete namespace istio-system &
 kubectl --context="${CTX_CLUSTER2}" delete namespace istio-system &
 kubectl --context="${CTX_CLUSTER1}" delete namespace sample &
 kubectl --context="${CTX_CLUSTER2}" delete namespace sample &
+
+kubectl --context="${CTX_CLUSTER1}" delete validatingwebhookconfigurations istiod-istio-system 
+kubectl --context="${CTX_CLUSTER2}" delete validatingwebhookconfigurations istiod-istio-system 
+
+kubectl --context="${CTX_CLUSTER1}" delete mutatingwebhookconfigurations istio-sidecar-injector
+kubectl --context="${CTX_CLUSTER2}" delete mutatingwebhookconfigurations istio-sidecar-injector
--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@ echo $CTX_CLUSTER2
 
 sleep 2
 
-rm -rf certs
-mkdir -p certs
-pushd certs
-
-make -f $ISTIO/tools/certs/Makefile.selfsigned.mk root-ca
-make -f $ISTIO/tools/certs/Makefile.selfsigned.mk cluster1-cacerts
-make -f $ISTIO/tools/certs/Makefile.selfsigned.mk cluster2-cacerts
-
-
-popd
+if [ -d certs ]; then
+  echo "using already created certs/ directory"
+else
+  mkdir -p certs
+  pushd certs
+  make -f $ISTIO/tools/certs/Makefile.selfsigned.mk root-ca
+  make -f $ISTIO/tools/certs/Makefile.selfsigned.mk cluster1-cacerts
+  make -f $ISTIO/tools/certs/Makefile.selfsigned.mk cluster2-cacerts
+  popd
+fi
 
 kubectl --context="${CTX_CLUSTER1}" create namespace istio-system
 kubectl --context="${CTX_CLUSTER1}" create secret generic cacerts -n istio-system \
@@ -29,12 +29,16 @@ kubectl --context="${CTX_CLUSTER2}" create secret generic cacerts -n istio-syste
       --from-file=certs/cluster2/root-cert.pem \
       --from-file=certs/cluster2/cert-chain.pem
 
-istioctl install --context="${CTX_CLUSTER1}" -f cluster1.yaml -y &
+istioctl install --manifests=$ISTIO/manifests --context="${CTX_CLUSTER1}" -f cluster1.yaml -y --set hub="$HUB" --set tag="$TAG" &
 PID1=$!
-istioctl install --context="${CTX_CLUSTER2}" -f cluster2.yaml -y &
+istioctl install --manifests=$ISTIO/manifests --context="${CTX_CLUSTER2}" -f cluster2.yaml -y --set hub="$HUB" --set tag="$TAG" &
 PID2=$!
 wait $PID1
 wait $PID2
+
+echo "wait a bit for cert injection... press any key to create cross-cluster secrets"
+read -n 1
+
 istioctl x create-remote-secret \
     --context="${CTX_CLUSTER1}" \
     --name=cluster1 | \
