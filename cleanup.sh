5f9bb60b27e10d8f030468323711851779e85977
Steven Landow <landow@google.com>
2021-05-05 12:56:34 -0700

    add cleanups

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,32 @@
 echo $CTX_CLUSTER1
 echo $CTX_CLUSTER2
+echo $CTX_CLUSTER3
 
 sleep 2
 
-kubectl --context="${CTX_CLUSTER1}" delete namespace istio-system &
-kubectl --context="${CTX_CLUSTER2}" delete namespace istio-system &
-kubectl --context="${CTX_CLUSTER1}" delete namespace sample &
-kubectl --context="${CTX_CLUSTER2}" delete namespace sample &
+declare -a PIDS
 
-kubectl --context="${CTX_CLUSTER1}" delete validatingwebhookconfigurations istiod-istio-system 
-kubectl --context="${CTX_CLUSTER2}" delete validatingwebhookconfigurations istiod-istio-system 
+for ctx in "${CTX_CLUSTER1}" "${CTX_CLUSTER2}" "${CTX_CLUSTER3}"; do
+  echo "cleaning $ctx"
+  kubectl --context="${ctx}" delete namespace istio-system &
+  PIDS+=($!)
+  kubectl --context="${ctx}" delete namespace sample &
+  PIDS+=($!)
+  kubectl --context="${ctx}" delete validatingwebhookconfigurations istiod-istio-system &
+  PIDS+=($!)
+  kubectl --context="${ctx}" delete mutatingwebhookconfigurations istio-sidecar-injector &
+  PIDS+=($!)
+  kubectl --context="${ctx}" delete clusterrole istiod-istio-system &
+  PIDS+=($!)
+  kubectl --context="${ctx}" delete clusterrolebinding istiod-istio-system &
+  PIDS+=($!)
+  kubectl --context="${ctx}" delete clusterrole istio-reader-istio-system &
+  PIDS+=($!)
+  kubectl --context="${ctx}" delete clusterrolebinding istio-reader-istio-system &
+  PIDS+=($!)
+done
 
-kubectl --context="${CTX_CLUSTER1}" delete mutatingwebhookconfigurations istio-sidecar-injector
-kubectl --context="${CTX_CLUSTER2}" delete mutatingwebhookconfigurations istio-sidecar-injector
+for pid in "${PIDS[@]}"; do
+  wait $pid
+done
+
